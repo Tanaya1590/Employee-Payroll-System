96674ae978466f43b7ca1d472eb848295f2c9517
tanaya1590 <tanayakarande0301@gmail.com>
2023-11-18 16:30:06 +0530

    Employee payroll system changes

---
+++ src/com/project/Main.java
@@ -0,0 +1,24 @@
+package com.project;
+
+public class Main {
+    public static void main(String[] args) {
+        PayrollSystem payrollSystem = new PayrollSystem();
+
+        FullTimeEmployee emp1 = new FullTimeEmployee("John Doe", 101, 5000.0);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Jane Smith", 102, 30, 15.0);
+        FullTimeEmployee emp3 = new FullTimeEmployee("Allen Hook", 103, 8000.0);
+
+        payrollSystem.addEmployee(emp1);
+        payrollSystem.addEmployee(emp2);
+        payrollSystem.addEmployee(emp3);
+
+        System.out.println("Initial Employee Details:");
+        payrollSystem.displayEmployees();
+
+        System.out.println("\nRemoving Employee...");
+        payrollSystem.removeEmployee(101);
+
+        System.out.println("\nRemaining Employee Details:");
+        payrollSystem.displayEmployees();
+    }
+}
